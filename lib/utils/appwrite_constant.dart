7345fa0978882ef2ce06c49746640da5b926ef1d
sbrunolima <bruno4study@outlook.com>
2023-08-31 00:06:48 -0300

    first commit

--- lib/utils/appwrite_constant.dart
+++
@@ -1,7 +0,0 @@
-class AppwriteConstants {
-  static String endPoint = 'https://cloud.appwrite.io/v1';
-  static String projectID = '64ee7faf101bd9489d48';
-  static String databaseID = '64eea525547f41b18e5d';
-  static String employeeCollectionID = '64eea5397c78eaeed190';
-  static String employeeBucketID = '64eea5c32924b0f3e0b2';
-}
